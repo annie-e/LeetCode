4414964d1f3008cdefa33ad576e7f27494f52879
annie-e <grace2hyun@gmail.com>
2026-01-15 22:33:57 -0500

    solution 3701

---
+++ java/Solution_3701.java
@@ -0,0 +1,21 @@
+public class Solution_3701 {
+	
+	/**
+	 * 3701. Compute Alternating Sum
+	 * @param nums
+	 * @return alternation sum
+	 */
+	public int alternatingSum(int[] nums) {
+        int sum = 0; 
+
+        for(int i = 0; i < nums.length; i++) {
+            if(i % 2 == 0) {
+                sum += nums[i];
+            } else {
+                sum -= nums[i];
+            }
+        }    
+
+        return sum;   
+    }
+}
