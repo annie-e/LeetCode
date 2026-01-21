bf41f2fc57499a496e1d6c1d64bd04493375ce1a
annie-e <grace2hyun@gmail.com>
2026-01-21 17:02:34 -0500

    solution 1470

---
+++ java/Solution_1470.java
@@ -0,0 +1,19 @@
+public class Solution_1470 {
+	
+	/**
+	 * 1470. Shuffle the Array
+	 * @param nums
+	 * @param n
+	 * @return shuffled array
+	 */
+	public int[] shuffle(int[] nums, int n) {
+		int[] ans = new int[nums.length];
+		
+		for(int i = 0; i < n; i++) {
+			ans[i * 2] = nums[i];
+			ans[i * 2 + 1] = nums[i + n];
+		}
+		
+		return ans;
+    }
+}
